7247380922d702aa5e1358789dd4a69a3ad892d5
Roslava Tsoncheva <raangelova97@gmail.com>
2025-01-03 13:28:39 +0200

    add entity constraints constants file

---
+++ GetMyTicket.Common/Constants/EntityConstraintsConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace GetMyTicket.Common.Constants
+{
+    public static class EntityConstraintsConstants
+    {
+        public const int EmailMaxLength = 20;
+        public const int NameMaxLength = 15;
+        public const int DescriptionMaxLength = 150;
+        public const int AddressMaxLength = 50;
+    }
+}

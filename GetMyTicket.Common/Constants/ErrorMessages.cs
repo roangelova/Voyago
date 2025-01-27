e6ae4860fe41f972f77307ba0fefd51a421ce1b3
Roslava Tsoncheva <raangelova97@gmail.com>
2025-01-27 13:13:14 +0200

    add wip: implement client login

--- GetMyTicket.Common/Constants/ErrorMessages.cs
+++ GetMyTicket.Common/Constants/ErrorMessages.cs
@@ -4,7 +4,7 @@
     {
         //API RESPONSE
 
-        public const string InvalidCredentials = "Invalid credentials. Please try again";
+        public const string InvalidCredentials = "Invalid credentials. Please try again!";
         public const string SomethingWentWrong = "Oops...Something went wrong.";
     }
 }
--- GetMyTicket.Frontend/getmyticket/src/components/account/Login.js
+++ GetMyTicket.Frontend/getmyticket/src/components/account/Login.js
@@ -1,10 +1,31 @@
+import { toast, ToastContainer } from "react-toastify";
+import { Account } from "../../services/accountService";
 
 function Login() {
 
-    return (
+    const Login = async (formData) => {
 
+        let email = formData.get('email');
+        let password = formData.get('password');
+
+        if (email == '' || password == '') {
+            toast.error('Emails and password required for login.')
+            return;
+        }
+
+        Account.login({ email, password }).then(res => {
+            console.log(res)
+            //TODO -> STORE TOKENS ADD ADD THEM TO AUTO REQ
+            toast.success('Logged in!')
+        }).catch(err => {
+            toast.error(err.response.data)
+        })
+    }
+
+    return (
         <div className="login__form">
-            <form >
+            <ToastContainer />
+            <form action={Login}>
                 <div className="login__row">
                     <label htmlFor="email" >
                         Email:
@@ -28,13 +49,13 @@ function Login() {
 
                 <button
                     className="login__btn"
-                    type="submit">
+                    type="submit"
+                >
                     Login
                 </button>
 
 
             </form>
-
         </div>
     )
 }

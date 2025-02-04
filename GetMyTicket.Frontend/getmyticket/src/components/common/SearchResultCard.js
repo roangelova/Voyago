24a73206812c38a7db063cde47903129fa98b38a
Roslava Tsoncheva <raangelova97@gmail.com>
2025-02-04 13:01:26 +0200

    create a simple Search result card component

--- GetMyTicket.Frontend/getmyticket/src/components/common/SearchResultCard.js
+++ GetMyTicket.Frontend/getmyticket/src/components/common/SearchResultCard.js
@@ -1,9 +1,30 @@
+import transavia_logo from '../../assets/images/transavia_logo.PNG'
+
 function SearchResultCard() {
 
     return (
-        <div className="search__result-card">
+        <div className="resultCard">
+            <div>
+
+                <div className="resultCard__provider">
+                    <img className="resultCard__provider-logo" src={transavia_logo} alt="Transportation provider logo" />
+                    <span>TransAvia</span>
+                </div>
+                <div className='resultCard__trip'>
+                    <span class="from">Varna</span>
+                    <span class="arrow">  →  </span>
+                    <span class="to">Munich</span>
+                </div>
+                <div className='connection'>
+                    <p>Departure: <span >10:30</span> </p>
+                    <p> Arrival: <span>12:40</span></p>
+                </div>
 
 
+            </div>
+            <div className='resultCard__price'>
+                <span>359 BGN</span>
+            </div>
         </div>
     )
 }

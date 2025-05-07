78950b3cc7b9801a1033a9b1c8a67fbdd7c50361
Roslava Tsoncheva <88967550+roangelova@users.noreply.github.com>
2025-05-07 15:49:19 +0300

    add FilterBy bookingStatus to Account/Bookings on frontend

--- GetMyTicket.Frontend/getmyticket/src/features/account/Bookings.js
+++ GetMyTicket.Frontend/getmyticket/src/features/account/Bookings.js
@@ -1,19 +1,57 @@
 import { formatDate } from "../../helpers";
+import { useState, useEffect, use } from "react";
 import { useGetUserBookings } from "../../services/bookingService";
+import FilterBy from "../common/FilterBy";
+import { useSearchParams } from "react-router-dom";
+
+const FILTER_PARAM = 'bookingStatus';
+
+const options = [
+    { key: 'Confirmed', value: 'Confirmed' },
+    { key: 'Canceled', value: 'Canceled' },
+]
 
 function Bookings() {
     const userId = sessionStorage.getItem('userId');
-    const { isPending, error, bookings } = useGetUserBookings(userId);
+    const { isPending, error, bookings = [] } = useGetUserBookings(userId);
+    const [filteredData, setFilteredData] = useState([]);
+
+
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    let updatedData = [...bookings];
+
+    useEffect(() => {
+        //set defaulot search params
+        searchParams.set('bookingStatus', 'Confirmed');
+        setSearchParams(searchParams)
+    }, [])
+
+    useEffect(() => {
+        //////FILTER
+        const filterByParam = searchParams.get(FILTER_PARAM);
+        console.log(filterByParam)
+        if (filterByParam !== 'all') {
+            updatedData = updatedData.filter(x => x.status === filterByParam)
+        }
+
+        setFilteredData(updatedData)
+    }, [searchParams, bookings])
 
     return (
 
-        //TTODO: ADD FILTER BY BOOKINGSTATUS
-
         <div className="bookings__container">
+            <div className="bookings__options">
+                <FilterBy
+                    title="Status"
+                    options={options}
+                    param={FILTER_PARAM}
+                />
+            </div>
             <table>
                 <thead>
 
-                    {!bookings || bookings.length === 0 ? <tr ><th className="bookings__noData">No bookings</th></tr> :
+                    {!filteredData || filteredData.length === 0 ? <tr ><th className="bookings__noData">No bookings</th></tr> :
                         <tr>
                             <th>Booking reference</th>
                             <th>From</th>
@@ -27,7 +65,7 @@ function Bookings() {
 
                 <tbody>
 
-                    {bookings?.map(b =>
+                    {filteredData?.map(b =>
                         <tr key={b.bookingId}>
                             <td>{b.bookingId}</td>
                             <td>{b.toCityName}</td>
@@ -35,7 +73,7 @@ function Bookings() {
                             <td>{formatDate(b.departureTime)}</td>
                             <td>{b.totalPrice} {b.currency}</td>
                             <td>{b.status}</td>
-                            <td><strong>Cancel | Details</strong></td>
+                            <td className="bookings__actions">{b.status === 'Confirmed' ? 'Cancel |': null } Details</td>
                         </tr>
                     )}
 

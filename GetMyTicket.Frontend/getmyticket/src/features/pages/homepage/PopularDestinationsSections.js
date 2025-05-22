5a7b02943e14043593fdfa6171d6805d74cc7951
Roslava Tsoncheva <88967550+roangelova@users.noreply.github.com>
2025-05-22 15:05:46 +0300

    add wip: start building the PopularDestinationsSection. js and its gallery

--- GetMyTicket.Frontend/getmyticket/src/features/pages/homepage/PopularDestinationsSections.js
+++ GetMyTicket.Frontend/getmyticket/src/features/pages/homepage/PopularDestinationsSections.js
@@ -1,11 +1,32 @@
-function PopularDestinationsSection(){
+import germany from "../../../assets/images/germany.jpg";
+import italy from "../../../assets/images/italy.jpg";
+import norway from "../../../assets/images/norway.jpg";
+import spain from "../../../assets/images/spain.jpg";
+import greece from "../../../assets/images/greece.jpg";
+import bulgaria from "../../../assets/images/bulgaria.jpg";
 
-    return(
-        <section className="popularDestinations">
-            <h2 className="heading--secondary margin-bottom--sm">Popular destinations</h2>
-            <p >Not sure where to go next? Check out where everyone’s heading</p>
-        </section>
-    )
+//TODO: ADD A SMALL POPUP ON HOVER WITH DETAILS -> semi-transparent black background
+
+
+function PopularDestinationsSection() {
+  return (
+    <section className="popularDestinations">
+      <h2 className="heading--secondary margin-bottom--sm">
+        Popular destinations
+      </h2>
+      <p className="margin-bottom--lg">Not sure where to go next? Check out where everyone’s heading</p>
+      <div className="popularDestinations__gallery margin-bottom--lg">
+        <div>
+          <img className="popularDestinations__gallery--img1" src={germany} alt="Destination image of Germany" />
+          <img className="popularDestinations__gallery--img2" src={italy} alt="Destination image of Italy" />
+          <img className="popularDestinations__gallery--img3" src={norway} alt="Destination image of Norway" />
+          <img className="popularDestinations__gallery--img4" src={spain} alt="Destination image of Spain" />
+          <img className="popularDestinations__gallery--img5" src={greece} alt="Destination image of Greece" />
+          <img className="popularDestinations__gallery--img6" src={bulgaria} alt="Destination image of Bulgaria" />
+        </div>
+      </div>
+    </section >
+  );
 }
 
 export default PopularDestinationsSection;

a1886b743c8e658648ab30c1e897c4f244033908
Roslava Tsoncheva <88967550+roangelova@users.noreply.github.com>
2025-05-27 17:27:48 +0300

    fix uneven logos size;

--- GetMyTicket.Frontend/getmyticket/src/features/services/SearchBar.js
+++ GetMyTicket.Frontend/getmyticket/src/features/services/SearchBar.js
@@ -8,114 +8,143 @@ import Spinner from "../../ui/Spinner";
 import { useQueryClient } from "@tanstack/react-query";
 
 const SearchBar = ({ LoginPopupVisibility }) => {
-    const { cities, error, isPending } = useGetCities();
-    const { mutate: getTrips } = useGetTrips();
+  const { cities, error, isPending } = useGetCities();
+  const { mutate: getTrips } = useGetTrips();
 
-    const queryClient = useQueryClient();
-    const navigate = useNavigate();
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
 
-    const [start, setStart] = useState('');
-    const [destination, setDestination] = useState('');
-    const [startDate, setStartDate] = useState('2025-05-10');
-    const [endDate, setEndDate] = useState('2025-05-18');
+  const [start, setStart] = useState("");
+  const [destination, setDestination] = useState("");
+  const [startDate, setStartDate] = useState("2025-05-10");
+  const [endDate, setEndDate] = useState("2025-05-18");
 
-    let params = useParams();
-    let searchType = params.searchType;
+  let params = useParams();
+  let searchType = params.searchType;
 
-    searchType === 'flights' ? searchType = 'Airplane'
-        : searchType === 'trains' ? searchType = 'Train'
-            : searchType == 'buses' ? searchType = 'Bus'
-                : searchType = 'all'
+  searchType === "flights"
+    ? (searchType = "Airplane")
+    : searchType === "trains"
+    ? (searchType = "Train")
+    : searchType == "buses"
+    ? (searchType = "Bus")
+    : (searchType = "all");
 
-    useEffect(() => {
-        if (cities?.length > 0) {
-            setStart(cities[0].cityId);
-            setDestination(cities[1].cityId);
-        }
-    }, [cities]);
-
-    if (error) toast.error("Failed to get cities.");
-
-    const handleSearch = () => {
-        getTrips({
-            "type": searchType,
-            "startDate": startDate,
-            "endDate": endDate,
-            "startCityId": start,
-            "endCityId": destination
-        },
-            {
-                //TODO: ADD VARIABLES
-                onSuccess: (trips) => {
-                    queryClient.setQueryData(['trips'], trips);;
-                    navigate("/search-results", { state: trips })
-                }
-            },
-            {
-                onError: (error) => {
-                    toast.error(error.message)
-                }
-            }
-        )
+  useEffect(() => {
+    if (cities?.length > 0) {
+      setStart(cities[0].cityId);
+      setDestination(cities[1].cityId);
     }
+  }, [cities]);
 
+  if (error) toast.error("Failed to get cities.");
 
-    const availableCitiesForStart = cities?.filter(city => city.cityId !== destination);
-    const availableCitiesForDestination = cities?.filter(city => city.cityId !== start);
+  const handleSearch = () => {
+    getTrips(
+      {
+        type: searchType,
+        startDate: startDate,
+        endDate: endDate,
+        startCityId: start,
+        endCityId: destination,
+      },
+      {
+        //TODO: ADD VARIABLES
+        onSuccess: (trips) => {
+          queryClient.setQueryData(["trips"], trips);
+          navigate("/search-results", { state: trips });
+        },
+      },
+      {
+        onError: (error) => {
+          toast.error(error.message);
+        },
+      }
+    );
+  };
 
-    if (isPending) return <Spinner />
+  const availableCitiesForStart = cities?.filter(
+    (city) => city.cityId !== destination
+  );
+  const availableCitiesForDestination = cities?.filter(
+    (city) => city.cityId !== start
+  );
 
-    return (
-        <div className={`searchBar__container ${LoginPopupVisibility ? 'blurred' : ''}`}>
-            <div className='searchBar__container--element'>
-                <select name="start" value={start} onChange={(e) => setStart(e.target.value)}>
-                    {availableCitiesForStart?.map((city) => (
-                        <option key={city.cityId} value={city.cityId}>
-                            {city.cityName}
-                        </option>
-                    ))}
-                </select>
-            </div>
+  if (isPending) return <Spinner />;
 
-            <div className='searchBar__container--element'>
-                <select name="destination" value={destination} onChange={(e) => setDestination(e.target.value)}>
-                    {availableCitiesForDestination?.map((city) => (
-                        <option key={city.cityId} value={city.cityId}>
-                            {city.cityName}
-                        </option>
-                    ))}
-                </select>
-            </div>
+  return (
+    <div
+      className={`searchBar__container ${
+        LoginPopupVisibility ? "blurred" : ""
+      }`}
+    >
+      <div className="searchBar__container--element">
+       
+          <select
+            name="start"
+            value={start}
+            onChange={(e) => setStart(e.target.value)}
+          >
+            {availableCitiesForStart?.map((city) => (
+              <option key={city.cityId} value={city.cityId}>
+                {city.cityName}
+              </option>
+            ))}
+          </select>
+      </div>
 
-            <div className='searchBar__container--element'>
-                <input
-                    name="startDate"
-                    type="date"
-                    value={startDate}
-                    onChange={(e) => setStartDate(e.target.value)}
-                >
-                </input>
-            </div>
+      <div className="searchBar__container--element">
+        <select
+          name="destination"
+          value={destination}
+          onChange={(e) => setDestination(e.target.value)}
+        >
+            <optgroup label="Germany">
+                
+          
+          {availableCitiesForDestination?.map((city) => (
+            <option key={city.cityId} value={city.cityId}>
+              {city.cityName}
+            </option>
+          ))} 
+           </optgroup>
+            <optgroup label="Bulgaria">
+                
+          
+          {availableCitiesForDestination?.map((city) => (
+            <option key={city.cityId} value={city.cityId}>
+              {city.cityName}
+            </option>
+          ))} 
+           </optgroup>
+        </select>
+      </div>
 
-            <div className='searchBar__container--element'>
-                <input
-                    name="endDate"
-                    type="date"
-                    value={endDate}
-                    onChange={(e) => setEndDate(e.target.value)}
-                >
-                </input>
-            </div>
+      <div className="searchBar__container--element">
+        <input
+          name="startDate"
+          type="date"
+          value={startDate}
+          onChange={(e) => setStartDate(e.target.value)}
+        ></input>
+      </div>
 
-            <div className='searchBar__container--element'>
-                <button onClick={handleSearch} className='searchBar__button'>
-                    Search
-                </button>
-            </div>
+      <div className="searchBar__container--element">
+        <input
+          name="endDate"
+          type="date"
+          value={endDate}
+          onChange={(e) => setEndDate(e.target.value)}
+        ></input>
+      </div>
 
-        </div>
-    )
-
-}
+      <div className="searchBar__container--element">
+        <button onClick={handleSearch} className="searchBar__button">
+          Search
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default SearchBar;

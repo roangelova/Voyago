aaa562bf324ff2c46cd49e6e8f05b6e7937a2a92
Roslava Tsoncheva <88967550+roangelova@users.noreply.github.com>
2025-05-27 15:20:09 +0300

    adjust the react-toastidy styling to match app and fix cramped text

--- GetMyTicket.Frontend/getmyticket/src/features/account/Login.js
+++ GetMyTicket.Frontend/getmyticket/src/features/account/Login.js
@@ -2,102 +2,98 @@ import { useEffect } from "react";
 
 import { toast } from "react-toastify";
 import { Account } from "../../services/accountService";
-import Cookies from 'js-cookie';
+import Cookies from "js-cookie";
 
 function Login({ setLoginPopupVisibility }) {
+  const Login = (formData) => {
+    let email = formData.get("email");
+    let password = formData.get("password");
 
-    const Login = (formData) => {
-
-        let email = formData.get('email');
-        let password = formData.get('password');
-
-        if (email == '' || password == '') {
-            toast.error('Emails and password required for login.')
-            return;
-        }
-
-        Account.login({ email, password }).then(res => {
-
-            sessionStorage.setItem("userId", res.userId);
-
-            Cookies.set('accessToken',
-                JSON.stringify(
-                    {
-                        accessToken: res.accessToken,
-                        tokenType: res.tokenType
-                    }),
-                {
-                    expires: new Date(res.accessTokenExpires),
-                    secure: true, sameSite: false
-                });
-
-            Cookies.set('refreshToken',
-                JSON.stringify(
-                    { refreshToken: res.refreshToken }),
-                {
-                    expires: new Date(res.refreshTokenExpires),
-                    secure: true, sameSite: false
-                });
-
-            toast.success('Logged in! Taking you to the homepage in 3, 2, 1..')
-
-            setTimeout(() => {
-                window.location.href = "/";
-            }
-                , 3000);
-        }).catch(err => {
-            toast.error(err.response?.data ?? 'There was an error logging you in.')
-        })
+    if (email == "" || password == "") {
+      toast.error("Email and password required for login.");
+      return;
     }
 
-    return (
-        <div className="login">
-            <div className="login__container">
-                <div className="login__iconBox">
-                    <svg
-                        onClick={() => setLoginPopupVisibility(false)}
-                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-1">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
-                    </svg>
+    Account.login({ email, password })
+      .then((res) => {
+        sessionStorage.setItem("userId", res.userId);
 
-                </div>
+        Cookies.set(
+          "accessToken",
+          JSON.stringify({
+            accessToken: res.accessToken,
+            tokenType: res.tokenType,
+          }),
+          {
+            expires: new Date(res.accessTokenExpires),
+            secure: true,
+            sameSite: false,
+          }
+        );
 
-                <form action={Login}>
-                    <div className="login__row">
-                        <label htmlFor="email" >
-                            Email:
-                        </label>
-                        <input
-                            type="text"
-                            name="email"
-                            placeholder="Email"
-                        >
-                        </input>
-                    </div>
+        Cookies.set(
+          "refreshToken",
+          JSON.stringify({ refreshToken: res.refreshToken }),
+          {
+            expires: new Date(res.refreshTokenExpires),
+            secure: true,
+            sameSite: false,
+          }
+        );
 
-                    <div className="login__row">
-                        <label htmlFor="password" >
-                            Password:               </label>
-                        <input
-                            type="password"
-                            placeholder="Password"
-                            name="password"
-                        >
-                        </input>
-                    </div>
+        toast.success("Logged in! Taking you to the homepage in 3, 2, 1..");
+        setTimeout(() => {
+          window.location.href = "/";
+        }, 3000);
+      })
+      .catch((err) => {
+        toast.error(err.response?.data ?? "There was an error logging you in.");
+      });
+  };
 
-                    <button
-                        className="login__btn"
-                        type="submit"
-                    >
-                        Login
-                    </button>
-
-
-                </form>
-            </div>
+  return (
+    <div className="login">
+      <div className="login__container">
+        <div className="login__iconBox">
+          <svg
+            onClick={() => setLoginPopupVisibility(false)}
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-1"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 18 18 6M6 6l12 12"
+            />
+          </svg>
         </div>
-    )
+
+        <form action={Login}>
+          <div className="login__row">
+            <label htmlFor="email">Email:</label>
+            <input type="text" name="email" placeholder="Email"></input>
+          </div>
+
+          <div className="login__row">
+            <label htmlFor="password">Password: </label>
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+            ></input>
+          </div>
+
+          <button className="login__btn" type="submit">
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
